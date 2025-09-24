430c1847042b717e266e5d04c05939fb34fb8fcb
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-09-24 16:28:30 -0300

    refactor(pet): resolve bug de regex onde não permitia cadastros de usuarios com nome contendo acentos.
    - Troca do tipo double para BigDecimal do atributo weight para melhor funcionamento

--- src/main/java/com/jvitorsaugusto/vetclinic_api/dtos/petsDtos/PetDto.java
+++ src/main/java/com/jvitorsaugusto/vetclinic_api/dtos/petsDtos/PetDto.java
@@ -1,33 +1,38 @@
 package com.jvitorsaugusto.vetclinic_api.dtos.petsDtos;
 
-import com.jvitorsaugusto.vetclinic_api.model.SpeciesModel;
+
 import com.jvitorsaugusto.vetclinic_api.model.enums.Gender;
+import com.jvitorsaugusto.vetclinic_api.model.enums.Species;
 import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
 
 public record PetDto(
 
         @NotBlank(message = "O nome do pet é obrigatório")
-        @Pattern(regexp = "[a-zA-Z ]+", message = "O nome completo não pode conter caracteres especiais ou números.")
+        @Pattern(regexp = "^[\\p{L} ]+$", message = "O nome completo não pode conter caracteres especiais ou números.")
         String petName,
 
         @NotBlank(message = "O endereço é obrigatório")
         String address,
 
-        @Pattern(regexp = "^\\d+([.,]\\d+)?$", message = "O idade só pode conter números.")
         @Max(value = 20, message = "A idade deve ser no máximo 20")
         int age,
 
-        @Pattern(regexp = "^\\d+([.,]\\d+)?$", message = "O peso só pode conter números.")
         @DecimalMin(value = "0.5", message = "O peso deve ser no mínimo 0.5kg.")
         @DecimalMax(value = "60.0", message = "O peso não pode exceder 60kg.")
-        double weight,
+        BigDecimal weight,
 
         @NotNull
         String race,
 
         @NotNull
-        SpeciesModel typeOfPet,
+        Species typeOfPet,
 
         @NotNull
-        Gender gender) {
+        Gender gender,
+
+        @NotNull
+        Long tutorId
+) {
 }
