19b8161f59af313eba05727f818a20cf814a595a
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-09-24 16:14:34 -0300

    feat(security): implementa UserDetailsService para carregar usuários
    - Cria o CustomUserDetailsService para buscar usuários do banco de dados
    pelo email
    - Adiciona a classe ModifyUserDetails, que adapta
    o UserModel para a interface UserDetails do Spring Security.

---
+++ src/main/java/com/jvitorsaugusto/vetclinic_api/service/ModifyUserDetails.java
@@ -0,0 +1,52 @@
+package com.jvitorsaugusto.vetclinic_api.service;
+
+import com.jvitorsaugusto.vetclinic_api.model.UserModel;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ModifyUserDetails implements UserDetails {
+
+    private UserModel userModel;
+
+    public ModifyUserDetails(UserModel userModel) {
+        this.userModel = userModel;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return userModel.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userModel.getEmail();
+    }
+}
