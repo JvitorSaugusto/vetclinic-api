a8c0cfbbf33d592885c796a850fc622395ec4b23
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-09-24 16:33:17 -0300

    feat(pet): inclui dados resumidos do tutor na resposta de Pet e atualiza o atributo specie para utilizar o tipo Enum
    - A propriedade specie nos DTOs e modelos de Pet foi alterada
    de String para um Enum, aumentando a segurança de tipo e a
    legibilidade do código.
    
    - A resposta da API para a entidade Pet agora inclui um objeto
    aninhado TutorRegisterDtoResponse com o ID e o nome do tutor.
    Isso evita que o cliente precise fazer uma requisição adicional
    para obter os dados básicos do dono do pet e evita expor o tutor model.

--- src/main/java/com/jvitorsaugusto/vetclinic_api/dtos/petsDtos/PetRegisterDtoResponse.java
+++ src/main/java/com/jvitorsaugusto/vetclinic_api/dtos/petsDtos/PetRegisterDtoResponse.java
@@ -1,18 +1,23 @@
 package com.jvitorsaugusto.vetclinic_api.dtos.petsDtos;
 
+import com.jvitorsaugusto.vetclinic_api.dtos.tutorsDtos.TutorRegisterDtoResponse;
 import com.jvitorsaugusto.vetclinic_api.model.PetModel;
+import com.jvitorsaugusto.vetclinic_api.model.enums.Species;
 
 public record PetRegisterDtoResponse(
         Long id,
         String petName,
-        String specieName
+        Species specie,
+        TutorRegisterDtoResponse tutor
+
 ) {
 
     public PetRegisterDtoResponse (PetModel model){
         this(
                 model.getId(),
                 model.getPetName(),
-                model.getTypeOfPet() != null ? model.getTypeOfPet().getNome() : "Não especificado"
+                model.getTypeOfPet(),
+                new TutorRegisterDtoResponse(model.getTutorModel().getId(), model.getTutorModel().getTutorName())
         );
 
     }
